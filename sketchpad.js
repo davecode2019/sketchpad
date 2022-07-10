0031e7f8b9da36ddf015218f6720c8ab0181d535
davecode2019 <dave.code2019@gmail.com>
2022-07-10 20:15:06 +0100

    Started work on resizable grid and some styling tweaks.

--- sketchpad.js
+++ sketchpad.js
@@ -1,12 +1,19 @@
 const gridbox = document.querySelector(".gridbox");
+const GRIDWIDTH = 600;
+let gridSize = 16;
+let squareSize = GRIDWIDTH / gridSize;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < gridSize; i++) {
   let row = document.createElement("div");
-  row.classList.add(`gridrow`);
+  row.classList.add("gridrow");
   gridbox.appendChild(row);
-  for (let j = 0; j < 16; j++) {
+  for (let j = 0; j < gridSize; j++) {
     let square = document.createElement("div");
-    square.classList.add(`square`);
+    square.classList.add("square");
+    square.setAttribute(
+      "style",
+      `width: ${squareSize}px; height: ${squareSize}px`
+    );
     // square.setAttribute("id", `${i},${j}`); Might not be needed
     row.appendChild(square);
   }
