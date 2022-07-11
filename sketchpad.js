6bb872d16cfd2ef52117a2384ac1d35ac19663ca
davecode2019 <dave.code2019@gmail.com>
2022-07-11 14:13:37 +0100

    Grid now resizable between 1 and 100 squares. Added pen up/down feature which means the mouse has to be down to draw.

--- sketchpad.js
+++ sketchpad.js
@@ -1,28 +1,57 @@
 const gridbox = document.querySelector(".gridbox");
-const GRIDWIDTH = 600;
-let gridSize = 16;
-let squareSize = GRIDWIDTH / gridSize;
+const GRIDWIDTH = 600; // Drawing area size in pixels (height is the same)
+let switchOn = false; // Pen default to off
+let gridSize = 100; // Amount of squares on each side of the grid
+let squareSize = GRIDWIDTH / gridSize; // Calculates the pixel size for each square within the drawing area
 
-for (let i = 0; i < gridSize; i++) {
-  let row = document.createElement("div");
-  row.classList.add("gridrow");
-  gridbox.appendChild(row);
-  for (let j = 0; j < gridSize; j++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    square.setAttribute(
-      "style",
-      `width: ${squareSize}px; height: ${squareSize}px`
-    );
-    // square.setAttribute("id", `${i},${j}`); Might not be needed
-    row.appendChild(square);
+const setSize = () => {
+  let userSize = prompt("Select grid size between 1 and 100:");
+  if (userSize > 0 && userSize <= 100) {
+    gridSize = userSize;
+    squareSize = GRIDWIDTH / gridSize;
+    pageSetup();
+  } else {
+    setSize();
   }
-}
+};
 
-const squares = document.querySelectorAll(".square");
+const pageSetup = () => {
+  // Clear any existing grid
+  gridbox.innerHTML = "";
+  // Draw grid
+  for (let i = 0; i < gridSize; i++) {
+    let row = document.createElement("div");
+    row.classList.add("gridrow");
+    gridbox.appendChild(row);
+    for (let j = 0; j < gridSize; j++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.setAttribute(
+        "style",
+        `width: ${squareSize}px; height: ${squareSize}px` // Square size adaptable dependant on user choice of grid size
+      );
+      row.appendChild(square);
+    }
+  }
+  // Make node list of grid elements
+  const squares = document.querySelectorAll(".square");
+  // Add event listeners to gid elements
+  squares.forEach((square) => {
+    square.addEventListener("mousedown", function () {
+      switchOn = true; // Pen on
+    });
+    square.addEventListener("mouseup", function () {
+      switchOn = false; // Pen off
+    });
+    square.addEventListener("mouseover", function (e) {
+      if (switchOn) {
+        e.target.style.backgroundColor = "red";
+      }
+    });
+  });
 
-squares.forEach((square) =>
-  square.addEventListener("mouseover", function (e) {
-    e.target.style.backgroundColor = "red";
-  })
-);
+  const gridsizeButton = document.querySelector(".gridsize-button");
+  gridsizeButton.addEventListener("click", setSize);
+};
+
+pageSetup();
